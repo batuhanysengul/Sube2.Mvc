a45f2c1f28b618d1c5fe70b3527d46283a677950
LAB4-PC <LAB4-PC@DESKTOP-FG3CP8I>
2022-06-01 16:20:10 +0300

    Add project files.

---
+++ Sube2.Mvc/Controllers/OgrenciController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sube2.Mvc.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sube2.Mvc.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult OgrenciEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult OgrenciEkle(Ogrenci ogr)
+        {
+            if (ogr != null)
+            {
+                using (var ctx = new OkulDbContext())
+                {
+                    ctx.Ogrenciler.Add(ogr);
+                    ctx.SaveChanges();
+                }
+            }
+            return View();
+        }
+
+        public IActionResult OgrenciListe()
+        {
+            List<Ogrenci> lst = null;
+            using (var ctx = new OkulDbContext())
+            {
+                lst = ctx.Ogrenciler.ToList();
+            }
+            return View(lst);
+        }
+    }
+}
